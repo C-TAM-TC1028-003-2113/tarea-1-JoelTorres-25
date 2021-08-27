1e740072eb8109fa6474afd29f11ce0b39f486be
JoelTorres-25 <89654949+JoelTorres-25@users.noreply.github.com>
2021-08-27 15:24:49 -0500

    Editorial Joel

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,10 @@
+import math
 def main():
     # escribe tu código abajo de esta línea
-    pass
-
-
+    palabras=int(input('Dame el número de palabras: '))
+    paginas= math.ceil(palabras/475)
+    costo= (paginas*60)-(paginas*60*.10) 
+    print('El costo de la publicación es:',costo)
+    
 if __name__ == '__main__':
     main()
